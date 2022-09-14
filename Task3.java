e03c0349a36314f7ad430fe0b851e48bf25000df
Dmitry Averyanov <averyanov-22@yandex.ru>
2022-09-14 13:41:34 +0300

    добавил скобки в условиях, переименовал переменные, проверил отступы, добавил проверку входящего значения на null в этот раз использовал просто if-else

--- Task3.java
+++ Task3.java
@@ -3,21 +3,28 @@ public class Task3 {
      * Task3
      * Реализовать функцию нечеткого поиска
       */
-    public static void fuzzySearch(String sub, String str){
+    public static void fuzzySearch(String target, String area){
         boolean answer = false;
 
-        for (int i = 0, j = 0; i < sub.length(); i++){
-
-            while (j < str.length() && sub.charAt(i) != str.charAt(j)) j++;
-
-            if(j < str.length() && sub.charAt(i) == str.charAt(j)) j++;
-
-            if(i == sub.length()-1) answer = true;
-
-            if(j == str.length() || i == sub.length()-1) break;
+        if(target != null && area != null){
+            for (int i = 0, j = 0; i < target.length(); i++){
+                while (j < area.length() && target.charAt(i) != area.charAt(j)) {
+                    j++;
+                }
+                if (j < area.length() && target.charAt(i) == area.charAt(j)) {
+                    j++;
+                }
+                if (i == target.length()-1) {
+                    answer = true;
+                }
+                if (j == area.length()) {
+                    break;
+                }
+            }
+            System.out.println(answer);
+        } else {
+            System.out.println("входящие данные равны null");
         }
-
-        System.out.println(answer);
     }
 
     public static void main(String[] args) {
@@ -27,5 +34,8 @@ public class Task3 {
         fuzzySearch("cartwheel", "cartwheel"); // true
         fuzzySearch("cwheeel", "cartwheel"); // false
         fuzzySearch("lw", "cartwheel"); // false
+        fuzzySearch(null, "cartwheel"); // false
+        fuzzySearch("lw", null); // false
+        fuzzySearch(null, null); // false
     }
 }
