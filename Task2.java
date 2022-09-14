ced0bdd662789d3d26cf12da2fe3cdfb6b3c0850
Dmitry Averyanov <averyanov-22@yandex.ru>
2022-09-14 13:39:03 +0300

    Добавил обработку значения null использовал try-catch

--- Task2.java
+++ Task2.java
@@ -10,19 +10,25 @@ public class Task2 {
          HashMap<Integer, Integer> differenceAndIndex = new HashMap<>();
          int[] result = new int[2];
 
-         for (int i = 0; i < ints.length; i++) {
-             if (differenceAndIndex.containsKey(ints[i])) {
-                 result[0] = differenceAndIndex.get(ints[i]);
-                 result[1] = ints[i];
-             } else
-                 differenceAndIndex.put(target - ints[i], ints[i]);
+         try {
+             for (int i = 0; i < ints.length; i++) {
+                 if (differenceAndIndex.containsKey(ints[i])) {
+                     result[0] = differenceAndIndex.get(ints[i]);
+                     result[1] = ints[i];
+                 } else
+                     differenceAndIndex.put(target - ints[i], ints[i]);
+             }
+         } catch (NullPointerException e) {
+             e.printStackTrace();
          }
          System.out.println(Arrays.toString(result));
      }
 
     public static void main(String[] args) {
+        int[] arr = new int[] {3, 4, 2, 7};
 
-        solutionTask2(new int[]{3, 4, 2, 7}, 10);
-
+        solutionTask2(arr, 10);
+        solutionTask2(new int[]{}, 10);
+        solutionTask2(null, 10);
     }
 }
